0e64d226038e935a2660845f42b03708084fa534
Ken Simpson <ksimpson@mailchannels.com>
2024-02-07 13:22:43 -0800

    Removing debugging traces; moving tensors to the same device

--- nodes.py
+++ nodes.py
@@ -8,16 +8,17 @@ def match_normalize(target_tensor, source_tensor, dimensions=4):
         raise ValueError("source_latent must have four dimensions")
     if len(source_tensor.shape) != dimensions:
         raise ValueError("target_latent must have four dimensions")
-    
-    logging.warning(f"target_tensor.shape = {target_tensor.shape}")
+
+    # Put everything on the same device
+    device = target_tensor.device
 
     # Calculate the mean and std of target tensor
-    tgt_mean = target_tensor.mean(dim=[2, 3], keepdim=True)
-    tgt_std = target_tensor.std(dim=[2, 3], keepdim=True)
+    tgt_mean = target_tensor.mean(dim=[2, 3], keepdim=True).to(device)
+    tgt_std = target_tensor.std(dim=[2, 3], keepdim=True).to(device)
     
     # Calculate the mean and std of source tensor
-    src_mean = source_tensor.mean(dim=[2, 3], keepdim=True)
-    src_std = source_tensor.std(dim=[2, 3], keepdim=True)
+    src_mean = source_tensor.mean(dim=[2, 3], keepdim=True).to(device)
+    src_std = source_tensor.std(dim=[2, 3], keepdim=True).to(device)
     
     # Normalize target tensor to have mean=0 and std=1, then rescale
     normalized_tensor = (target_tensor.clone() - tgt_mean) / tgt_std * src_std + src_mean
@@ -57,15 +58,12 @@ class ImageMatchNormalize:
     @torch.no_grad()
     def image_match_normalize(self, source_image, target_image):
         # image shape is [B, H, W, C], but the normalize function needs [B, C, H, W]
-        logging.warning(f"source.shape = {source_image.shape}")
         source = source_image.permute(0,3,1,2)
         target = target_image.permute(0,3,1,2)
         
         normalized = match_normalize(target, source, dimensions=4)
 
-        logging.warning(f"normalized.shape = {normalized.shape}")
         normalized_image = normalized.permute(0,2,3,1)
-        logging.warning(f"normalized_image.shape = {normalized_image.shape}")
         return (normalized_image,)
     
 NODE_CLASS_MAPPINGS = {
